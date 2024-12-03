4081da7efe43fd46f7b2b010fd0ead985cc1dc4f
Ubuntu <ubuntu@ip-172-31-19-80.us-west-1.compute.internal>
2024-12-03 01:47:29 +0000

    committing from ubuntu aws terminal

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello, Elastic Beanstalk!");
+    res.send("Hello, EC2!");
 });
 
 app.listen(PORT, () => {
